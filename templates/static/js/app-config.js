974bf2df501e357b5f3f1275affc006cca804cc6
minhvuong3322 <vuongthm4994@ut.edu.vn>
2025-06-23 20:38:18 +0700

    .

--- templates/static/js/app-config.js
+++ templates/static/js/app-config.js
@@ -1,9 +1,9 @@
 const API_CONFIG = {
     BE_PORT: 5000,
-    BE_HOST: '192.168.123.72', // hoặc '192.168.56.1' tùy theo mạng bạn đang dùng
 
     getBaseUrl: function () {
-        return `https://${this.BE_HOST}:${this.BE_PORT}`;
+        const hostname = window.location.hostname;
+        return `https://${hostname}:${this.BE_PORT}`;
     },
 
     getApiUrl: function () {
